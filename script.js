d67958ca7a986cc2ab1d149c8787107caca6b714
debopriyadey <debodey158@gmail.com>
2021-06-28 02:47:27 +0530

    complete project v1

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var amount = principal + interest*years;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    var interest = principal * years * rate / 100;
+    var amount = principal + interest * years;
+    var year = new Date().getFullYear() + parseInt(years);
+    if (principal <= 0) {
+        alert('Enter a positive number')
+    } else {
+        document.getElementById("result").innerHTML = "\<br\>\<br\>If you deposit " + "<mark>" + principal + "</mark>" +
+            ",\<br\>at an interest rate of " + "<mark>" + rate + "</mark>" +
+            "%.\<br\>You will receive an amount of " + "<mark>" + interest + "</mark>" +
+            ",\<br\>in the year " + "<mark>" + year + "</mark>" + ".\<br\>"
+    }
 }
 
-function updateRate() 
-{
+function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval+'%';
+    document.getElementById("rate_val").innerText = rateval + '%';
 }
